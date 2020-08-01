eeb1ba6386405adc2b0a93177428f6df3d261808
David Garcia <david.garcia@adevinta.com>
2020-08-01 13:12:09 +0200

    update test and fixtures

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,13 +1,19 @@
 const fs = require('fs');
 const clicks = JSON.parse(fs.readFileSync('./clicks.json'));
 
-const {reduceClicksList, getClickHour} = require('../app/reduceClicksList')
+const {reduceClicksList, getClickHour, getMaxEntriesByIp} = require('../app/reduceClicksList')
 const originalListFixture = require('./fixtures/originalListFixture.json')
+const maxEntriesByIpFixture = require('./fixtures/maxEntriesByIpFixture.json')
 const {timestamp} = require('./fixtures/timestamp')
+const {MAX_CLICKS_BY_IP} = require('../app/config')
 
 describe('Given an array of clicks', () => {
-  test('Given a timestamp string should get its numeric hour value', () => {
+  test('Should get its timestamp and return its numeric hour value', () => {
     expect(getClickHour(timestamp)).toBe(2)
+  })
+
+  test.only('Should return the max list of clicks by IP', () => {
+    expect(getMaxEntriesByIp(clicks)).toStrictEqual(maxEntriesByIpFixture)
   })
 
   test('Should return the same list', () => {
--- app/reduceClicksList.js
+++ app/reduceClicksList.js
@@ -1,19 +1,42 @@
-const MAX_CLICKS_BY_IP = 10;
+const timestamp = require("../__test__/fixtures/timestamp")
 
 // Get click hour
-const getClickHour = timestamp => new Date(timestamp).getHours()
+const getClickHour = timestamp => 
+  new Date(timestamp)
+    .getHours()
+    // .toString()
+    // .padStart(2,'0')
+    // .padEnd(8,':00:00')
 
-// Return the number of entries limited by MAX_CLICKS_BY_IP constant
-const getMaxEntriesByIp = data => {
+const getMostExpensiveClick = data => 
+  data.sort((a,b) => b.amount - a.amount)
 
+
+// A custom sort helper function
+const sortByKey = data => {
+  let sortOrder = 1
+
+  if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+  }
+
+  return function (a,b) {
+    sortOrder == -1
+      ? b[property].localeCompare(a[property])
+      : a[property].localeCompare(b[property])
+    }        
 }
+
+
 
 let reduceClicksList = clicks => {
   let reducedClickListArr = []
-
+  return clicks
 }
 
 module.exports = {
-  reduceClicksList,
-  getClickHour
+  getClickHour,
+  getMaxEntriesByIp,
+  reduceClicksList
 }
